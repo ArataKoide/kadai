db15f1e002fb807d904653c3ae5673ab7abc1243
arata <arata58@icloud.com>
2017-06-26 17:16:31 +0900

    プロコン（自由）課題

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int x = int.Parse(Console.ReadLine())/*入力した数字（文字）を数値に変換して代入*/;
+                string y = "";
+
+                while(x != 0)/*xが０じゃないとき繰り返す*/
+                {
+                    y = "ヽ(∴｀┏Д┓´)ﾉ彡#" + y;
+                    x = x - 1;
+                }
+                Console.WriteLine(y);
+                Console.WriteLine("------------------------------終わり------------------------------");
+            }
+        }
+    }
+}
